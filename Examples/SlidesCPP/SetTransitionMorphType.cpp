830d31cc96b0f04ecd94be3b321fc711c8b646d8
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-03-06 23:51:23 +0500

    Added examples based on 19.2

---
+++ Examples/SlidesCPP/SetTransitionMorphType.cpp
@@ -0,0 +1,39 @@
+#include <Export/SaveFormat.h>
+#include <DOM/Presentation.h>
+#include <DOM/ISlideCollection.h>
+#include <DOM/IShapeCollection.h>
+#include <DOM/SlideShowTransition/TransitionType.h>
+#include <DOM/SlideShowTransition/TransitionMorphType.h>
+#include <DOM/SlideShowTransition/IMorphTransition.h>
+#include <DOM/ISlideShowTransition.h>
+#include <DOM/ShapeType.h>
+#include <DOM/AutoShape.h>
+
+#include <DOM/ITextFrame.h>
+#include <system/string.h>
+#include <system/IO/file.h>
+
+
+#include "SlidesExamples.h"
+
+using namespace Aspose::Slides;
+using namespace Aspose::Slides::SlideShow;
+
+using namespace System;
+
+void SetTransitionMorphType()
+{
+	//ExStart:SetTransitionMorphType
+	const String inputPath = u"../templates/presentation.pptx";
+	const String outPath = u"../out/presentation-out.pptx";
+	auto presentation = System::MakeObject<Presentation>(u"presentation.pptx");
+
+	presentation->get_Slides()->idx_get(0)->get_SlideShowTransition()->set_Type(Aspose::Slides::SlideShow::TransitionType::Morph);
+
+	auto morphTransition = System::DynamicCast<Aspose::Slides::SlideShow::IMorphTransition>(presentation->get_Slides()->idx_get(0)->get_SlideShowTransition()->get_Value());
+	morphTransition->set_MorphType(Aspose::Slides::SlideShow::TransitionMorphType::ByWord);
+
+	presentation->Save(u"presentation-out.pptx", Aspose::Slides::Export::SaveFormat::Pptx);
+
+	//ExEnd:SetTransitionMorphType
+}
---
+++ Examples/SlidesCPP/SupportOfMorphTransition.cpp
@@ -0,0 +1,48 @@
+#include <Export/SaveFormat.h>
+#include <DOM/Presentation.h>
+#include <DOM/ISlideCollection.h>
+#include <DOM/IShapeCollection.h>
+#include <DOM/SlideShowTransition/TransitionType.h>
+#include <DOM/ISlideShowTransition.h>
+#include <DOM/ShapeType.h>
+#include <DOM/AutoShape.h>
+#include <DOM/ITextFrame.h>
+#include <system/string.h>
+#include <system/IO/file.h>
+
+
+#include "SlidesExamples.h"
+
+using namespace Aspose::Slides;
+using namespace Aspose::Slides::SlideShow;
+
+using namespace System;
+
+void SupportOfMorphTransition()
+{
+	//ExStart:SupportOfMorphTransition
+
+	const String outPath = u"../out/presentation-out.pptx";
+	auto presentation = System::MakeObject<Presentation>();
+	auto autoshape = System::DynamicCast<Aspose::Slides::AutoShape>(presentation->get_Slides()->idx_get(0)->get_Shapes()->AddAutoShape(Aspose::Slides::ShapeType::Rectangle, 100, 100, 400, 100));
+	
+	
+	autoshape->get_TextFrame()->set_Text(u"Test text");
+	presentation->get_Slides()->AddClone(presentation->get_Slides()->idx_get(0));
+	
+	auto x = presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->get_X();
+	auto y = presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->get_Y();
+	auto width = presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->get_Width();
+	auto height = presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->get_Height();
+
+	presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->set_X(x + 100);
+	presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->set_Y(y + 50);
+	presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->set_Width(width + 200);
+	presentation->get_Slides()->idx_get(1)->get_Shapes()->idx_get(0)->set_Height(height + 10);
+
+	presentation->get_Slides()->idx_get(1)->get_SlideShowTransition()->set_Type(Aspose::Slides::SlideShow::TransitionType::Morph);
+
+	presentation->Save(outPath, Aspose::Slides::Export::SaveFormat::Pptx);
+
+	//ExEnd:SupportOfMorphTransition
+}
