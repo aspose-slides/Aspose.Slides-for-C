62d3823f7a2c07b72d284c0dcf63a64a8b818993
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-04-11 22:25:12 +0500

    Updated examples w.r.t 19.3

---
+++ Examples/SlidesCPP/ExportToHTMLWithResponsiveLayout.cpp
@@ -0,0 +1,18 @@
+#include <Export/SaveFormat.h>
+#include <DOM/Presentation.h>
+#include <Export/HtmlOptions.h>
+#include<Export/ResponsiveHtmlController.h>
+#include <Export/HtmlFormatter.h>
+
+#include "SlidesExamples.h"
+using namespace Aspose::Slides;
+using namespace System;
+
+void ExportToHTMLWithResponsiveLayout() {
+	//ExStart:ExportToHTMLWithResponsiveLayout
+	SharedPtr<Presentation> presentation = MakeObject<Presentation>(u"SomePresentation.pptx");
+	SharedPtr<Aspose::Slides::Export::HtmlOptions> saveOptions = MakeObject <Aspose::Slides::Export::HtmlOptions>();
+	saveOptions->set_SvgResponsiveLayout(true);
+	presentation->Save(u"../out/SomePresentation-out.html", Export::SaveFormat::Html, saveOptions);
+	//ExEnd:ExportToHTMLWithResponsiveLayout
+}
---
+++ Examples/SlidesCPP/HighlightText.cpp
@@ -0,0 +1,25 @@
+#include <Export/SaveFormat.h>
+#include <DOM/Presentation.h>
+#include <DOM/ISlideCollection.h>
+#include <DOM/IShapeCollection.h>
+#include <DOM/IAutoShape.h>
+#include <DOM/TextHighlightingOptions.h>
+#include <DOM/AutoShape.h>
+#include <DOM/ITextFrame.h>
+
+#include "SlidesExamples.h"
+using namespace Aspose::Slides;
+using namespace System;
+
+void HighlightText() {
+	//ExStart:HighlightText
+	System::SharedPtr<Presentation> presentation = System::MakeObject<Presentation>(u"SomePresentation.pptx");
+	// highlighting all words 'important'
+	(System::DynamicCast<Aspose::Slides::AutoShape> (presentation->get_Slides()->idx_get(0)->get_Shapes()->idx_get(0)))->get_TextFrame()->HighlightText(u"important", System::Drawing::Color::get_LightBlue());
+	auto options = System::MakeObject<TextHighlightingOptions>();
+	options->set_WholeWordsOnly(true);
+	// highlighting all separate 'the' occurrences
+	(System::DynamicCast <Aspose::Slides::AutoShape>(presentation->get_Slides()->idx_get(0)->get_Shapes()->idx_get(0)))->get_TextFrame()->HighlightText(u"the", System::Drawing::Color::get_Violet(), options);
+	presentation->Save(u"../out/SomePresentation-out.pptx", Aspose::Slides::Export::SaveFormat::Pptx);
+	//ExEnd:HighlightText
+}
