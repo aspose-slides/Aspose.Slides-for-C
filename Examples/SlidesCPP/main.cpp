60d83d50562dec1c33a98e0f9588b26df7bf98af
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-10-18 09:49:42 +0500

    Update main.cpp

--- Examples/SlidesCPP/main.cpp
+++ Examples/SlidesCPP/main.cpp
@@ -77,6 +77,7 @@ int main(int argc, const char argv[])
 				//BestImagesCompressionRatioforPDF();
 		        //ExportToHTMLWithResponsiveLayout();
 		        //CovertToPDFWithProgressUpdate();
+		         //SaveToStream();
 
 		#pragma endregion
 
